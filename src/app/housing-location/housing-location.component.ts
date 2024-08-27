466267134aefdb8f170461c61e224eb7c53fb7d4
julie.saurat <julie.saurat.s@gmail.com>
2024-08-27 17:50:30 -0400

    details pages routing

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../interfaces';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   template: `
     <section class="listing">
       <img
-        class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
-        crossorigin
+      class="listing-photo"
+      [src]="housingLocation.photo"
+      alt="Exterior photo of {{ housingLocation.name }}"
+      crossorigin
       />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrls: ['./housing-location.component.css']
